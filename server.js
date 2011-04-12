51f3b0398154f3b545814df5c35d661e6794416d
Stephan Seidt <evilhackerdude@gmail.com>
2011-04-12 21:05:02 +0200

    Added voting

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var url = require('url');
 var nodes = {
 	0: {
 		content: 'I hate trees',
-		children: [1,2]
+		children: [1,2],
+		points: -1
 	},
 	1: {
 		content: 'I hate you',
 		children: [3]
 	},
 	2: {
-		content: 'Trees must go, I agree. Major distraction.',
-		children: []
+		content: 'Trees must go, I agree. Major distraction.'
 	},
 	3: {
 		content: 'Try being more polite, will you? He is not a tree after all.',
@@ -24,33 +24,57 @@ var nodes = {
 		children: [5]
 	},
 	5: {
-		content: 'Tree troll. Get lost.',
-		children: []
+		content: 'Tree troll. Get lost.'
 	}
 };
 
 var render = function(nodes, rootId){
 	var node = nodes[rootId];
 	var content = node.content;
+	var points = node.points || 0;
 	var permalink = '<a href="/' + rootId + '">link</a>';
-	var children = node.children.map(function(childId){
+	var upvote = '<a href="/' + rootId + '/up">up</a>';;
+	var downvote = '<a href="/' + rootId + '/down">down</a>';;
+	var children = (node.children || []).map(function(childId){
 		return render(nodes, childId);
 	}).join('');
-	var html = '<li>' + [content, permalink].join(' | ') + '<ul>' + children + '</ul></li>';
+	var html = '<li>' + [points, content, permalink, upvote, downvote].join(' | ') + '<ul>' + children + '</ul></li>';
 	return html;
+};
+
+var upvote = function(nodes, rootId){
+	if (typeof nodes[rootId].points === 'undefined') nodes[rootId].points = 0;
+	nodes[rootId].points++;
+};
+
+var downvote = function(nodes, rootId){
+	if (typeof nodes[rootId].points === 'undefined') nodes[rootId].points = 0;
+	nodes[rootId].points--;
 };
 
 var server = http.createServer(function(req, res){
 	var path = url.parse(req.url).pathname;
-	var rootId = path.slice(1);
-	
+	var segments = path.slice(1).split('/');
+	var rootId = segments[0];
+	var action = segments[1];
+
 	if (typeof nodes[rootId] === 'undefined'){
 		res.writeHead(404);
 		res.end();
 		return;
 	}else{
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('<ul>' +render(nodes, rootId) + '</ul>');	
+		if(typeof action === 'undefined'){
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end('<ul>' + render(nodes, rootId) + '</ul>');	
+		}else if(action === 'up'){
+			upvote(nodes, rootId);
+			res.writeHead(302, {'Location': req.headers.referer});
+			res.end();
+		}else if(action === 'down'){
+			downvote(nodes, rootId);
+			res.writeHead(302, {'Location': req.headers.referer});
+			res.end();
+		}
 	}
 });
 
